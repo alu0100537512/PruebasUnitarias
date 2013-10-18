9d4a8de1d4ec8d37efc8e4f96ef77d1ecf668a05
Enrique Tejera Gonzalez <alu0100537512@ull.edu.es>
2013-10-18 01:34:55 +0100

    pruebas unitarias añadidas

--- tc_racional.rb
+++ tc_racional.rb
@@ -5,4 +5,24 @@ require "test/unit"
 
 class Test_Fraccion < Test::Unit::TestCase
 
+	def test_suma
+		assert_equal([14, 5], Fraccion.new(4,5).suma(8,4))
+		assert_equal([14, 3], Fraccion.new(8,3).suma(2))
+	end
+	
+	def test_resta
+		assert_equal([-6, 5], Fraccion.new(4,5).resta(8,4))
+		assert_equal([2, 3], Fraccion.new(8,3).resta(2))
+	end
+
+	def test_producto
+		assert_equal([8, 5], Fraccion.new(4,5).producto(8,4))
+		assert_equal([16, 3], Fraccion.new(8,3).producto(2))
+	end
+
+	def test_division
+		assert_equal([2, 5], Fraccion.new(4,5).division(8,4))
+		assert_equal([4, 3], Fraccion.new(8,3).division(2))
+	end
+
 end
